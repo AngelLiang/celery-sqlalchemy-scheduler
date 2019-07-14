a58e82f85f85c81ec2533fbff8b9e98e25646b1c
AngelLiang <pl01665077@163.com>
2019-07-14 11:10:47 +0800

    chore: Updated examples

--- examples/base/tasks.py
+++ examples/base/tasks.py
@@ -18,12 +18,22 @@ Run Beat::
 
 """
 import os
+import time
 import platform
 from datetime import timedelta
+
 from celery import Celery
 from celery import schedules
 
 from celery_sqlalchemy_scheduler.schedulers import DatabaseScheduler  # noqa
+
+from dotenv import load_dotenv
+dotenv_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), '.env')
+if os.path.exists(dotenv_path):
+    load_dotenv(dotenv_path, override=True)
+
+ECHO_EVERY_MINUTE = os.getenv('ECHO_EVERY_MINUTE', '0')
+ECHO_EVERY_HOUR = os.getenv('ECHO_EVERY_HOUR', '8')
 
 if platform.system() == 'Windows':
     # Celery在Windows环境下运行需要设置这个变量，否则调用任务会报错
@@ -31,6 +41,7 @@ if platform.system() == 'Windows':
 
 backend = 'rpc://'
 broker_url = 'amqp://guest:guest@localhost:5672//'
+
 
 # 如果数据库修改了下面的schedule，beat重启后数据库会被下面的配置覆盖
 beat_schedule = {
@@ -43,6 +54,16 @@ beat_schedule = {
         'task': 'tasks.add',
         'schedule': schedules.crontab('*', '*', '*'),
         'args': (1, 2)
+    },
+    'echo-every-hours': {
+        'task': 'tasks.echo',
+        'schedule': schedules.crontab(ECHO_EVERY_MINUTE, '*', '*'),
+        'args': ('echo-every-hours',)
+    },
+    'echo-every-days': {
+        'task': 'tasks.echo',
+        'schedule': schedules.crontab(ECHO_EVERY_MINUTE, ECHO_EVERY_HOUR, '*'),
+        'args': ('echo-every-days',)
     },
 }
 
@@ -69,15 +90,15 @@ celery = Celery('tasks',
                 backend=backend,
                 broker=broker_url)
 
-config = dict(
-    beat_schedule=beat_schedule,
-    beat_scheduler=beat_scheduler,
-    beat_max_loop_interval=beat_max_loop_interval,
-    beat_dburi=beat_dburi,
+config = {
+    'beat_schedule': beat_schedule,
+    # 'beat_scheduler': beat_scheduler,
+    'beat_max_loop_interval': beat_max_loop_interval,
+    'beat_dburi': beat_dburi,
 
-    timezone=timezone,
-    worker_max_tasks_per_child=worker_max_tasks_per_child
-)
+    'timezone': timezone,
+    'worker_max_tasks_per_child': worker_max_tasks_per_child
+}
 
 celery.conf.update(config)
 
