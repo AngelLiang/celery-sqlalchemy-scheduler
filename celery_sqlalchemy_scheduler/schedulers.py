a7eb04505e40c88d9c3e51bfeca846d9a2eeef98
AngelLiang <pl01665077@163.com>
2019-08-31 08:43:38 +0800

    refactor: Deleted literals.py

--- celery_sqlalchemy_scheduler/schedulers.py
+++ celery_sqlalchemy_scheduler/schedulers.py
@@ -21,7 +21,18 @@ from .models import (
     CrontabSchedule, IntervalSchedule,
     SolarSchedule,
 )
-from .literals import DEFAULT_MAX_INTERVAL, DEFAULT_BEAT_DBURI, ADD_ENTRY_ERROR
+
+# This scheduler must wake up more frequently than the
+# regular of 5 minutes because it needs to take external
+# changes to the schedule into account.
+DEFAULT_MAX_INTERVAL = 5  # seconds
+
+DEFAULT_BEAT_DBURI = 'sqlite:///schedule.db'
+
+ADD_ENTRY_ERROR = """\
+Cannot add entry %r to database schedule: %r. Contents: %r
+"""
+
 
 session_manager = SessionManager()
 # session = session_manager()
